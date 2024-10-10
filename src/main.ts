d7b5900ce09ba6b38f3604b032b47593b7501c26
Arshdeep Singh <arshdeep59645@gmail.com>
2024-10-10 09:41:02 -0400

    Checked builtin provideRouter and listed all the routes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter, Route} from "@angular/router";
+
+const routes: Route[] = [
+  { path: '', component: "" },
+  { path: 'modify-list-item', component: "" },
+  { path: '**', component: PageNotFoundComponent }
+];
+
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
