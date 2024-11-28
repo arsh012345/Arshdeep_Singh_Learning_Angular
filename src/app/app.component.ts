1ea51febc57d79f4a339b21555d2b55fb21d2dd3
Arshdeep Singh <arshdeep59645@gmail.com>
2024-11-28 17:20:43 -0500

    Added angular material, put the data in different material design like mat-card,mat-list, mat-card-title, mat-list-item. Also use custom mat to make it appear good.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,14 @@ import { RouterModule } from '@angular/router';
 import { Employee } from './data/mock-content';
 import { employee } from './data/employee.model';
 import {HHighlightDirective} from "./h-highlight.directive";
+import {MatCard, MatCardContent, MatCardTitle} from "@angular/material/card";
+import {MatList, MatListItem} from "@angular/material/list";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, NgForOf, NgIf, NgClass, HHighlightDirective , CommonModule, DatePipe,UpperCasePipe,CurrencyPipe],
+  imports: [RouterOutlet, RouterModule, NgForOf, MatCard, MatCardTitle, NgIf, NgClass, HHighlightDirective, CommonModule, DatePipe, UpperCasePipe, CurrencyPipe, MatCardContent, MatListItem, MatList, MatIcon],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
